22f8865c8e948e58f3aff810f57be224f0377e1b
leviipope <leventep71@gmail.com>
2025-12-26 22:44:38 +0100

    lesson3: css styling: external, modules, inline

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,20 @@
-import Card from "./Card.jsx"
+// HOW TO STYLE REACT COMPONENTS WITH CSS
+// --------------------------------------
+// (not including external frameworks or preprocessors)
+
+// 1. EXTERNAL - for global styles or small projects
+// 2. MODULES - for individual components
+// 3. INLINE - small components with minimal styling
+
+import Button from "./Button";
+import ButtonModule from "./ButtonModule/ButtonModule";
 
 function App() {
 
     return(
         <>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Button/>
+            <ButtonModule/>
         </>
     );
 }
---
+++ my-react-app/src/ButtonModule/ButtonModule.jsx
@@ -0,0 +1,9 @@
+import styles from './ButtonModule.module.css'
+
+function ButtonModule(){
+    return(
+        <button className={styles.button}>Im module</button>
+    );
+}
+
+export default ButtonModule
